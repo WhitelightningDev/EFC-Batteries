eed568d0360ddc56b3f3fabeda8615db6716ec78
Daniel <danielmommsen2@gmail.com>
2025-03-17 19:02:38 +0200

    added a margin in the times column

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,8 +5,23 @@ import { MaterialModule } from '../../shared/material/material.module';
   selector: 'app-home',
   imports: [MaterialModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+  openMaps(): void {
+    const userAgent = navigator.userAgent || navigator.vendor;
+
+    // Check if the device is an iPhone
+    if (/iPhone|iPad|iPod/i.test(userAgent)) {
+      // Open in Apple Maps on iPhone
+      window.location.href = "maps://?q=476+Steve+Biko+Rd,+Gezina,+Pretoria,+0084";
+    } else if (/android/i.test(userAgent)) {
+      // Open in Google Maps on Android
+      window.location.href = "https://www.google.com/maps?q=476+Steve+Biko+Rd,+Gezina,+Pretoria,+0084";
+    } else {
+      // For other devices, fallback to the web link
+      window.open("https://www.google.com/maps?q=476+Steve+Biko+Rd,+Gezina,+Pretoria,+0084", "_blank");
+    }
+  }
 }
